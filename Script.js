6e3b40f2b632c500030f1b71d83de718c7e54c7b
Zehna <86805879+Zehna@users.noreply.github.com>
2024-12-31 00:17:19 +0700

    Add files via upload

---
+++ Script.js
@@ -0,0 +1,5 @@
+// script.js
+document.getElementById('welcomeButton').addEventListener('click', function() {
+    // Ganti URL dengan link TikTok yang Anda inginkan
+    window.location.href = "https://vt.tiktok.com/ZS6S8b7Tb/"; 
+});
